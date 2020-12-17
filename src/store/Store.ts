23b4761241fe902c18c944dd5eea5434108fd646
zbz <nnzbz@21cn.com>
2020-12-17 22:40:28 +0800

    * 更新菜单数据

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,7 +2,7 @@ import * as mobx from 'mobx';
 import SysStore from './SysStore';
 import UserStore from './UserStore';
 
-mobx.configure({ enforceActions: true });
+mobx.configure({ enforceActions: 'observed' });
 
 export const sysStore = new SysStore();
 export const userStore = new UserStore();
