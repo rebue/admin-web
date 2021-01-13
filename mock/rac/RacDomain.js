bd3fc7f2a3acadbafc68369585fae5e5f147334f
zbz <nnzbz@21cn.com>
2021-01-13 12:06:21 +0800

    + 完成增删改查的模拟

--- mock/rac/RacDomain.js
+++ mock/rac/RacDomain.js
@@ -1,37 +1,94 @@
 const moment = require('moment');
-const Mock = require('mockjs');
-const Random = Mock.Random;
+const { parse } = require('url');
 
 const list = [
     {
-        id: '1',
-        signInName: 'admin',
-        signInPswd: '52569c045dc348f12dfc4c85000ad832',
-        signInNickname: '平台管理员',
-        sign:
-            'eyJhbGciOiJIUzUxMiJ9.eyJuYmYiOjE2MDc1ODk4MzAsImlzcyI6Inpib3NzIiwiZXhwIjoxNjA3NTg5ODMzLCJpYXQiOjE2MDc1ODk4MzAsInVzZXJJZCI6IjEifQ.NodxTh-rNCGwmkR1BhyebNl9Eeh0zV4v6KcaqBH6h2Kaqj4gKDMmVHstXCqsNKnLPKoOlcIYWW5BTVV94_wRRw',
+        id: 'default',
+        name: '默认领域',
+        remark: '系统和用户的默认领域',
+    },
+    {
+        id: 'platform',
+        name: '平台领域',
+        remark: '平台管理领域',
     },
 ];
 
-const listRacUser = () => list;
-const curUserInfo = {
-    result: 1,
-    msg: '获取当前用户信息成功',
-    extra: {
-        id: list[0].id,
-        nickname: list[0].signInNickname,
-        isTester: false,
-        menus: ['/base/rac-domain'],
-    },
-};
-
-const findRacUserBySignInName = signInName => list.find(item => item.signInName === signInName);
+const listAllRacDomain = () => list;
 
 module.exports = {
-    findRacUserBySignInName,
-    listRacUser,
+    listAllRacDomain,
     routes: {
-        'GET /rac/user/list': listRacUser,
-        'GET /rac/user/get-cur-user-info': curUserInfo,
+        /** 添加 */
+        'POST /rac/domain': (req, res, u, b) => {
+            const body = (b && b.body) || req.body;
+            list.push(body);
+            return res.json({
+                result: 1,
+                msg: '添加成功',
+                extra: {
+                    id: body.id,
+                },
+            });
+        },
+        /** 修改 */
+        'PUT /rac/domain': (req, res, u, b) => {
+            const body = (b && b.body) || req.body;
+            const replacedIndex = list.findIndex(item => item.id === body.id);
+            if (replacedIndex !== -1) {
+                list.splice(replacedIndex, 1, body);
+                return res.json({
+                    result: 1,
+                    msg: '修改成功',
+                });
+            } else {
+                return res.json({
+                    result: -1,
+                    msg: '修改失败，找不到要修改的记录',
+                });
+            }
+        },
+        'DELETE /rac/domain': (req, res, u) => {
+            let url = u;
+            if (!url || Object.prototype.toString.call(url) !== '[object String]') {
+                url = req.url;
+            }
+            const params = parse(url, true).query;
+            const removedIndex = list.findIndex(item => item.id === params.id);
+            if (removedIndex !== -1) {
+                list.splice(removedIndex, 1);
+                return res.json({
+                    result: 1,
+                    msg: '删除成功',
+                });
+            } else {
+                return res.json({
+                    result: -1,
+                    msg: '删除失败，找不到要删除的记录',
+                });
+            }
+        },
+        'GET /rac/domain/get-by-id': (req, res, u) => {
+            let url = u;
+            if (!url || Object.prototype.toString.call(url) !== '[object String]') {
+                url = req.url;
+            }
+            const params = parse(url, true).query;
+
+            const eo = list.find(item => item.id === params.id);
+            if (eo) {
+                return res.json(eo);
+            } else {
+                return res.json({});
+            }
+        },
+        /** 查询所有记录 */
+        'GET /rac/domain/list-all': {
+            result: 1,
+            msg: '查询列表成功',
+            extra: {
+                list: listAllRacDomain(),
+            },
+        },
     },
 };
