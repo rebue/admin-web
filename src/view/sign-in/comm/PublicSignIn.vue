6b8d10d275b6d191b702361e41050b5d719a8344
yuanman-one <1782271387@qq.com>
2021-07-05 17:56:11 +0800

    *封装登录组件

---
+++ src/view/sign-in/comm/PublicSignIn.vue
@@ -0,0 +1,169 @@
+<template>
+    <!-- 只需要通过 rules 属性传入约定的验证规则，
+                并将 FormItem 的 prop 属性设置为需校验的字段名即可。
+                校验规则参见 https://github.com/yiminghe/async-validator -->
+    <a-tabs default-active-key="1" @change="callback">
+        <a-tab-pane key="1" tab="账号密码登录">
+            <div v-if="show" class="sign">
+                <a-form-model ref="form" class="form" :model="form" :rules="rules">
+                    <a-form-model-item prop="accountName" class="sign">
+                        <a-input v-autofocus v-model.trim="form.accountName" placeholder="请输入登录账号">
+                            <template #prefix><a-icon type="account"/></template>
+                        </a-input>
+                    </a-form-model-item>
+                    <a-form-model-item prop="signInPswd">
+                        <a-input-password v-model="form.signInPswd" placeholder="请输入登录密码">
+                            <template #prefix><a-icon type="key"/></template>
+                        </a-input-password>
+                    </a-form-model-item>
+                    <a-button :loading="loading" type="primary" block @click="doSubmit" class="sign">登录</a-button>
+                </a-form-model>
+            </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="微信扫码登录">
+            <div id="wx-login-box" style="width: 350px;"></div>
+        </a-tab-pane>
+    </a-tabs>
+</template>
+
+<script src="http://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"></script>
+<script>
+import md5 from 'crypto-js/md5';
+import { setSysId } from '@/util/cookie';
+import { racSignInApi } from '@/api/Api';
+export default {
+    components: {
+        //
+    },
+    props: {
+        sysId: {
+            type: String,
+            default: () => '',
+        },
+    },
+    data() {
+        this.api = racSignInApi;
+        return {
+            /** 登录后要中转的地址 */
+            redirect: undefined,
+            /** 定义当前登录页面所对应的系统 */
+            //sysId: SysIdDic.OpsAdminWeb,
+            loading: false,
+            show: true,
+            form: {
+                accountName: '',
+                signInPswd: '',
+            },
+            rules: {
+                accountName: [
+                    { required: true, message: '请输入登录账号', trigger: 'blur', transform: val => val.trim() },
+                ],
+                signInPswd: [
+                    { required: true, message: '请输入登录密码', trigger: 'blur', transform: val => val.trim() },
+                ],
+            },
+        };
+    },
+    watch: {
+        visible(val) {
+            if (val) {
+                this.$emit('show');
+                this.$nextTick(() => {
+                    //如果不延时，搜索框可能还未渲染完成，会找不到input报错
+                    setTimeout(() => {
+                        this.$focus(this.$refs.modal);
+                    }, 100);
+                });
+            } else {
+                this.$emit('close');
+            }
+        },
+    },
+    mounted() {
+        const s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.src = 'http://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js';
+        document.body.appendChild(s);
+        this.$nextTick(() => {
+            this.get_wx_qrcode();
+        });
+    },
+    methods: {
+        changeShow() {
+            this.show = !this.show;
+            this.switchLogin();
+        },
+        callback(key) {
+            this.show = !this.show;
+            console.log(key);
+            if (key == 2) {
+                this.switchLogin();
+            }
+        },
+        get_wx_qrcode() {
+            // let call_back_domain_name = window.location.href,
+            //     domain_name_length = call_back_domain_name.indexOf('#') - 1,
+            //     DDNS = call_back_domain_name.substring(0, domain_name_length),
+            //     DDNS_resolution = encodeURIComponent(DDNS);
+            const obj = new WxLogin({
+                self_redirect: false,
+                id: 'wx-login-box',
+                appid: 'wx25d6c6c863c8629a',
+                scope: 'snsapi_login',
+                redirect_uri: encodeURIComponent(
+                    'https://khadmin.cocmis.cn/host/pcLogin?type=wxredirect&host_url=' + window.BASE_URL
+                ),
+                state: Math.ceil(Math.random() * 1000),
+                style: 'black',
+                href:
+                    'data:text/css;base64,LmltcG93ZXJCb3ggLnFyY29kZXsNCgl3aWR0aDogMTAwJTsNCgltYXJnaW46IDA7DQoJYm9yZGVyOiBub25lOw0KfQ0KLmltcG93ZXJCb3ggLnRpdGxlew0KCWRpc3BsYXk6IG5vbmU7DQp9DQouaW1wb3dlckJveCAuaW5mb3sNCgl3aWR0aDogMTAwJTsNCn0=',
+            });
+            console.log('dddd', obj);
+        },
+        switchLogin() {
+            this.get_wx_qrcode();
+        },
+        doSubmit() {
+            console.log('doSubmit!');
+            this.loading = true;
+
+            this.$refs.form.validate(valid => {
+                if (valid) {
+                    this.api
+                        .signInByAccountName({
+                            sysId: this.sysId,
+                            accountName: this.form.accountName,
+                            signInPswd: md5(this.form.signInPswd).toString(),
+                        })
+                        .then(() => {
+                            setSysId(this.sysId);
+                            window.location.href = this.redirect ? '#' + this.redirect : '';
+                        })
+                        .finally(() => (this.loading = false));
+                } else {
+                    this.$nextTick(() => {
+                        this.$focusError(); // 设置焦点到第一个提示错误的输入框
+                        this.loading = false;
+                    });
+                }
+            });
+        },
+    },
+};
+</script>
+<style scoped>
+.form {
+    width: 280px;
+    height: 450px;
+}
+.sign {
+    margin-top: 40px;
+    margin-bottom: 20px;
+}
+.footer {
+    height: 40px;
+    padding: 10px;
+    color: white;
+    text-align: center;
+}
+</style>
--- src/view/sign-in/ops/OpsSignIn.vue
+++ src/view/sign-in/ops/OpsSignIn.vue
@@ -4,24 +4,9 @@
             <div class="header-center">运营管理<a-divider type="vertical" />登录</div>
         </div>
         <div class="main">
-            <a-card title="账号密码登录" class="login-card">
-                <!-- 只需要通过 rules 属性传入约定的验证规则，
-                并将 FormItem 的 prop 属性设置为需校验的字段名即可。
-                校验规则参见 https://github.com/yiminghe/async-validator -->
-                <a-form-model ref="form" class="form" :model="form" :rules="rules">
-                    <a-form-model-item prop="accountName">
-                        <a-input v-autofocus v-model.trim="form.accountName" placeholder="请输入登录账号">
-                            <template #prefix><a-icon type="account"/></template>
-                        </a-input>
-                    </a-form-model-item>
-                    <a-form-model-item prop="signInPswd">
-                        <a-input-password v-model="form.signInPswd" placeholder="请输入登录密码">
-                            <template #prefix><a-icon type="key"/></template>
-                        </a-input-password>
-                    </a-form-model-item>
-                    <a-button :loading="loading" type="primary" block @click="doSubmit">登录</a-button>
-                </a-form-model>
-            </a-card>
+            <div class="login-card">
+                <public-sign-in :sysId="sysId" class="form"></public-sign-in>
+            </div>
         </div>
         <div class="footer">相关系统链接: <router-link to="/sign-in/platform">平台管理系统</router-link></div>
         <div class="footer">&copy;2020 zbz, Rebue. All rights reserved.</div>
@@ -29,12 +14,13 @@
 </template>
 
 <script>
-import md5 from 'crypto-js/md5';
 import { SysIdDic } from '@/dic/SysIdDic';
-import { racSignInApi } from '@/api/Api';
-import { setSysId } from '@/util/cookie';
+import PublicSignIn from '@/view/sign-in/comm/PublicSignIn.vue';
 
 export default {
+    components: {
+        PublicSignIn,
+    },
     data() {
         return {
             /** 登录后要中转的地址 */
@@ -42,18 +28,6 @@ export default {
             /** 定义当前登录页面所对应的系统 */
             sysId: SysIdDic.OpsAdminWeb,
             loading: false,
-            form: {
-                accountName: '',
-                signInPswd: '',
-            },
-            rules: {
-                accountName: [
-                    { required: true, message: '请输入登录账号', trigger: 'blur', transform: val => val.trim() },
-                ],
-                signInPswd: [
-                    { required: true, message: '请输入登录密码', trigger: 'blur', transform: val => val.trim() },
-                ],
-            },
         };
     },
     watch: {
@@ -65,31 +39,7 @@ export default {
         },
     },
     methods: {
-        doSubmit() {
-            console.log('doSubmit!');
-            this.loading = true;
-
-            this.$refs.form.validate(valid => {
-                if (valid) {
-                    racSignInApi
-                        .signInByAccountName({
-                            sysId: this.sysId,
-                            accountName: this.form.accountName,
-                            signInPswd: md5(this.form.signInPswd).toString(),
-                        })
-                        .then(() => {
-                            setSysId(this.sysId);
-                            window.location.href = this.redirect ? '#' + this.redirect : '';
-                        })
-                        .finally(() => (this.loading = false));
-                } else {
-                    this.$nextTick(() => {
-                        this.$focusError(); // 设置焦点到第一个提示错误的输入框
-                        this.loading = false;
-                    });
-                }
-            });
-        },
+        //
     },
 };
 </script>
@@ -123,9 +73,10 @@ export default {
         align-items: center; /* 垂直居中 */
 
         .login-card {
-            margin-top: -200px;
+            //margin-top: 150px;
             width: 450px;
-            height: 300px;
+            //height: 300px;
+            background-color: aliceblue;
             display: flex;
             flex-direction: column;
             justify-content: center; /* 水平居中 */
@@ -142,5 +93,10 @@ export default {
         color: white;
         text-align: center;
     }
+}
+.impowerBox .qrcode {
+    width: 280px;
+    margin: 0;
+    border: none;
 }
 </style>
