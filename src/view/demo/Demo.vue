5a8a2068f81344637ffd46d952eafbf62c08a233
lq <1213811243@qq.com>
2021-08-31 11:33:25 +0800

    登录成功页面

--- src/view/demo/Demo.vue
+++ src/view/demo/Demo.vue
@@ -1,16 +1,13 @@
 <template>
     <div>
         <div>
-            {{ msg }}
-        </div>
-        <div>
-            {{ accessToken }}
+            登录成功
         </div>
     </div>
 </template>
 
 <script>
-// http://127.0.0.1:10080/oap-svr/oidc/authorize?scope=openid&response_type=code&client_id=unified-auth&state=RECOMMENDED&redirect_uri=http%3A%2F%2F127.0.0.1%3A13080%2Forp-svr%2Foidc%2Fcallback
+// http://127.0.0.1:13080/oap-svr/oidc/authorize?scope=openid&response_type=code&client_id=unified-auth&state=RECOMMENDED&redirect_uri=http%3A%2F%2F127.0.0.1%3A13080%2Forp-svr%2Foidc%2Fcallback
 import request from '@/util/request';
 
 export default {
@@ -22,40 +19,8 @@ export default {
         };
     },
     created() {
-        const code = this.getCode();
-        if (code) {
-            this.msg = 'code: ' + code;
-        } else {
-            this.msg = '无code参数';
-            return;
-        }
-
-        const redirectUri = encodeURIComponent('http://127.0.0.1:13080/admin-web/#/demo');
-        request
-            .get({
-                url: '/orp-svr/oidc/callback?code=' + code + '&redirectUri=' + redirectUri,
-            })
-            .then(result => {
-                if (result.result === 1) {
-                    this.accessToken = result.extra;
-                }
-            });
     },
     methods: {
-        getCode() {
-            const idx = window.location.href.indexOf('?');
-            if (idx < 0) {
-                return '';
-            }
-            const queryParams = window.location.href.split('?')[1].split('&');
-            for (let i = 0; i < queryParams.length; ++i) {
-                const kv = queryParams[i].split('=');
-                if (kv[0] === 'code') {
-                    return kv[1];
-                }
-            }
-            return '';
-        },
     },
 };
 </script>
