2208f561639b91e0573cac00cbd6c01badcb0553
钟珍梅 <761780789@qq.com>
2021-09-16 17:28:33 +0800

    微信扫码

--- src/view/app/security-center/Wechat.vue
+++ src/view/app/security-center/Wechat.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <wx-login-code :option="option"></wx-login-code>
+        <wx-login-code :option="option" v-if="false"></wx-login-code>
     </div>
 </template>
 <script>
 import WxLoginCode from './WXLoginCode.vue';
+import request from '@/util/request';
+
 export default {
     name: 'app-security-center-wechat',
     components: {
@@ -14,7 +16,7 @@ export default {
         return {
             option: {
                 self_redirect: false,
-                appid: 'wx25d6c6c863c8629a',
+                appid: process.env.VUE_APP_VX_CODE_APPID,
                 scope: 'snsapi_login',
                 redirect_uri: encodeURIComponent('https://khadmin.cocmis.cn/host/pcLogin?type=wxredirect'),
                 state: Math.ceil(Math.random() * 1000),
@@ -25,6 +27,9 @@ export default {
             },
         };
     },
+    mounted() {
+        this.getQrcode();
+    },
     methods: {
         ok() {
             console.log('--submit');
@@ -32,6 +37,20 @@ export default {
             //then
             this.callback && this.callback();
             this.closeDialog();
+        },
+        getQrcode() {
+            const redirectUri = `${process.env.VUE_APP_VX_REDIRECT_URL}/orp-svr/orp/get-user-info/wechat-open/${process.env.VUE_APP_VX_CODE_APPID}`;
+            return request
+                .get({
+                    url: `/orp-svr/orp/get-auth-url/wechat-open/${process.env.VUE_APP_VX_CODE_APPID}`,
+                    params: {
+                        redirectUri,
+                    },
+                })
+                .then(ro => {
+                    console.log('---res', ro.detail);
+                    window.open(ro.detail);
+                });
         },
     },
 };
