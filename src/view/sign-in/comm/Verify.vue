1e39b9e192c864587dbec833989ff95b091f8cbc
钟珍梅 <761780789@qq.com>
2021-09-22 16:26:59 +0800

    弹窗的形式展示验证码

---
+++ src/view/sign-in/comm/Verify.vue
@@ -0,0 +1,27 @@
+<template>
+    <Verify
+        ref="verify"
+        mode="fixed"
+        :captcha-type="'blockPuzzle'"
+        :img-size="{ width: '400px', height: '200px' }"
+        @success="success"
+        @error="error"
+    />
+</template>
+<script>
+import Verify from '@/component/rebue/verifition/Verify.vue';
+export default {
+    components: {
+        Verify,
+    },
+    methods: {
+        success(res) {
+            this.handleVerifySuccess(res);
+            this.closeDialog();
+        },
+        error() {
+            this.handleVerifyError();
+        },
+    },
+};
+</script>
