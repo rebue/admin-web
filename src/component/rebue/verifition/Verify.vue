286881c630f09972e1a60c7f0ea4a0ffc24ec1e8
钟珍梅 <761780789@qq.com>
2021-09-28 17:14:03 +0800

    验证码 样式优化

--- src/component/rebue/verifition/Verify.vue
+++ src/component/rebue/verifition/Verify.vue
@@ -362,8 +362,7 @@ export default {
         -webkit-box-sizing: content-box;
         -moz-box-sizing: content-box;
         box-sizing: content-box;
-        border-top: 1px solid #ddd;
-        border-bottom: 1px solid #ddd;
+        border: 1px solid #ddd;
         border-radius: 3px;
         position: relative;
     }
