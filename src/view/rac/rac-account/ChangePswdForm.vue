04d49ca20ebbcd52af81c15c96cee2cde94cc5af
钟珍梅 <761780789@qq.com>
2021-11-12 11:23:32 +0800

    管理员修改账号密码使用填写密码组件

--- src/view/rac/rac-account/ChangePswdForm.vue
+++ src/view/rac/rac-account/ChangePswdForm.vue
@@ -8,22 +8,15 @@
         @show="handleShow"
         @ok="handleOk"
     >
-        <a-form-model ref="form" :model="model" :rules="rules" v-bind="formLayout">
-            <a-form-model-item key="signInPswd" label="登录密码" prop="signInPswd" :help="help">
-                <a-input-password
-                    v-autofocus
-                    v-model.trim="model.signInPswd"
-                    placeholder="请输入登录密码"
-                    autocomplete="new-password"
-                />
-            </a-form-model-item>
-            <a-form-model-item key="signInPswdAgain" label="再次确认" prop="signInPswdAgain">
-                <a-input-password
-                    v-model.trim="model.signInPswdAgain"
-                    placeholder="请再次输入登录密码(确认)"
-                    autocomplete="new-password"
-                />
-            </a-form-model-item>
+        <a-form-model ref="form" :model="model" v-bind="formLayout">
+            <input-password key="signInPswd" :value.sync="model.signInPswd" label="登录密码" prop="signInPswd" />
+            <input-password-again
+                key="signInPswdAgain"
+                :value.sync="model.signInPswdAgain"
+                :target="model.signInPswd"
+                label="再次确认"
+                prop="signInPswdAgain"
+            />
         </a-form-model>
     </base-modal>
 </template>
@@ -31,10 +24,13 @@
 <script>
 import BaseModal from '@/component/rebue/BaseModal.vue';
 import { racAccountApi, racDicApi } from '@/api/Api';
-
+import InputPassword from '@/view/forget-password/widgets/InputPassword.vue';
+import InputPasswordAgain from '@/view/forget-password/widgets/InputPasswordAgain.vue';
 export default {
     components: {
         BaseModal,
+        InputPassword,
+        InputPasswordAgain,
     },
     props: {
         record: {
@@ -43,68 +39,6 @@ export default {
         },
     },
     data() {
-        this.rules = {
-            signInPswd: [
-                {
-                    required: true,
-                    // message: '请输入登录密码',
-                    trigger: 'blur',
-                    validator: (rule, value, callback) => {
-                        if (value === undefined) value = '';
-                        value = value.trim();
-                        if (value.length < this.passwordMinLength) {
-                            callback(new Error(`请最少输入${this.passwordMinLength}位数密码`));
-                            return;
-                        }
-                        //字典项里设置的包括多少种字符
-                        if (this.passwordCharacter == 2) {
-                            const pattern = /((?=.*[a-z])(?=.*\d)|(?=[a-z])(?=.*[#@!~%^&*])|(?=.*\d)(?=.*[#@!~%^&*]))[a-z\d#@!~%^&*]/i;
-                            if (!pattern.test(value)) {
-                                callback(new Error(`请最少输入2种字符密码格式`));
-                                return;
-                            }
-                        }
-                        if (this.passwordCharacter == 3) {
-                            const pattern = /^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_]+$)(?![a-z0-9]+$)(?![a-z\W_]+$)(?![0-9\W_]+$)[a-zA-Z0-9\W_]/;
-                            if (!pattern.test(value)) {
-                                callback(new Error(`请最少输入3种字符密码格式`));
-                                return;
-                            }
-                        }
-                        if (this.passwordCharacter == 4) {
-                            const pattern = /(?=.*[a-z])(?=.*\d)(?=.*[#@!~%^&*])[a-z\d#@!~%^&*]/i;
-                            if (!pattern.test(value)) {
-                                callback(new Error(`请最少输入4种字符密码格式`));
-                                return;
-                            }
-                        }
-                        callback();
-                    },
-                },
-            ],
-            signInPswdAgain: [
-                {
-                    required: true,
-                    trigger: 'blur',
-                    validator: (rule, value, callback) => {
-                        if (value === undefined) value = '';
-                        value = value.trim();
-
-                        if (value === '') {
-                            callback(new Error('请再次输入登录密码(确认)'));
-                            return;
-                        }
-
-                        if (value !== this.model.signInPswd) {
-                            callback(new Error('两次输入的登录密码不相同'));
-                            return;
-                        }
-
-                        callback();
-                    },
-                },
-            ],
-        };
         this.formLayout = {
             labelCol: {
                 xs: { span: 24 },
@@ -121,27 +55,9 @@ export default {
                 signInPswd: '',
                 signInPswdAgain: '',
             },
-            passwordMinLength: 6,
-            passwordCharacter: 2,
         };
     },
-    mounted() {
-        this.getbyIdFun();
-    },
     methods: {
-        getbyIdFun() {
-            const params = 'levelProtect';
-            racDicApi.getByDicKey(params).then(ro => {
-                ro.extra.dicItems?.map(item => {
-                    if (item.dicItemKey == 'passwordMinLength') {
-                        this.passwordMinLength = item.dicItemValue;
-                    } else if (item.dicItemKey == 'passwordCharacter') {
-                        this.passwordCharacter = item.dicItemValue;
-                    }
-                    this.help = `登录密码由${this.passwordMinLength}~20位字符组成，包含至少${this.passwordCharacter}种以上字母、数组、符号以上组合，区分大小写`;
-                });
-            });
-        },
         handleShow() {
             this.$nextTick(() => {
                 this.model = {};
