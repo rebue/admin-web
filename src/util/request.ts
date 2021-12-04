8de9a6527d5574910302af3fea2317c7096a3ba8
yuanman <1782271387@qq.com>
2021-12-04 14:40:58 +0800

    *请求延长

--- src/util/request.ts
+++ src/util/request.ts
@@ -30,7 +30,7 @@ const codeMessage = {
 const instance = axios.create({
     baseURL: requestBaseUrl, // url = base url + request url
     // withCredentials: true, // send cookies when cross-domain requests
-    timeout: 5000, // request timeout
+    timeout: 30000, // request timeout
 });
 
 // request interceptor
