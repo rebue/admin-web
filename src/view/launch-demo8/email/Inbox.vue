b958eb3f74dc43e5d549a3f33794129f63200a65
hgz <2326375069@qq.com>
2021-12-06 17:55:49 +0800

    系统8的1.3-1.7表格页

---
+++ src/view/launch-demo8/email/Inbox.vue
@@ -0,0 +1,139 @@
+// 写信页面
+<template>
+    <fragment>
+        <base-manager ref="baseManager">
+            <template #managerCard>
+                <crud-table
+                    ref="crudTable"
+                    :commands="tableCommands"
+                    :actions="tableActions"
+                    :columns="columns"
+                    :api="api"
+                    :scrollX="600"
+                    :defaultPagination="false"
+                    :rowSelection="{}"
+                >
+                </crud-table>
+            </template>
+        </base-manager>
+    </fragment>
+</template>
+
+<script>
+import BaseManager from '@/component/rebue/BaseManager';
+import CrudTable from '@/component/rebue/CrudTable.vue';
+import { racRealmApi } from '@/api/Api';
+
+export default {
+    name: 'signupConf',
+    components: {
+        BaseManager,
+        CrudTable,
+    },
+    data() {
+        this.api = racRealmApi;
+        const columns = [
+            {
+                dataIndex: 'no',
+                title: '#',
+                width: 50,
+                fixed: 'left',
+                scopedSlots: { customRender: 'serial' },
+            },
+            {
+                dataIndex: 'n',
+                title: '邮件',
+                width: 100,
+            },
+            {
+                dataIndex: 'title',
+                title: '标题',
+                ellipsis: true,
+            },
+            {
+                dataIndex: 'sendDate',
+                title: '发送时间',
+                ellipsis: true,
+            },
+            {
+                dataIndex: 'Sender',
+                title: '发送人',
+                ellipsis: true,
+            },
+            {
+                dataIndex: 'enclosure',
+                title: '附件',
+                ellipsis: true,
+            },
+            {
+                dataIndex: 'operation',
+                title: '操作',
+                width: 150,
+                fixed: 'right',
+                scopedSlots: { customRender: 'action' },
+            },
+        ];
+
+        this.tableCommands = [
+            {
+                buttonType: 'primary',
+                title: '阅读',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                title: '放入废件箱',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                title: '标记为已读',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                title: '标记为未读',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                title: '打印',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                title: '删除',
+                onClick: this.handleAdd,
+            },
+        ];
+
+        this.tableActions = [
+            {
+                type: 'a',
+                title: '回复',
+                onClick: record => this.handleEdit(record),
+            },
+            {
+                type: 'a',
+                title: '转发',
+                onClick: record => this.handleEdit(record),
+            },
+            {
+                type: 'confirm',
+                title: '删除',
+                confirmTitle: '你确定要删除本条记录吗?',
+                onClick: record => this.handleDel(record),
+            },
+        ];
+        return {
+            columns,
+        };
+    },
+    mounted() {
+        //
+    },
+    methods: {
+        //
+    },
+};
+</script>
---
+++ src/view/launch-demo8/email/write.vue
@@ -0,0 +1,48 @@
+// 写信页面
+<template>
+    <fragment>
+        <base-manager ref="baseManager">
+            <template #managerCard>
+                <a-form-model :label-col="{ span: 3 }" :wrapper-col="{ span: 12 }">
+                    <a-form-model-item label="发送人:">
+                        <a-input placeholder="系统管理员" disabled="true" />
+                    </a-form-model-item>
+                    <a-form-model-item label="接收人:">
+                        <a-input />
+                    </a-form-model-item>
+                    <a-form-model-item label="标题:">
+                        <a-input />
+                    </a-form-model-item>
+                    <a-form-model-item label="描述:">
+                        <a-textarea :rows="4" />
+                    </a-form-model-item>
+                    <a-form-model-item label="附件:">
+                        <a-button>上传</a-button>
+                    </a-form-model-item>
+                </a-form-model>
+            </template>
+        </base-manager>
+    </fragment>
+</template>
+
+<script>
+import BaseManager from '@/component/rebue/BaseManager';
+import { racRealmApi } from '@/api/Api';
+
+export default {
+    name: 'signupConf',
+    components: {
+        BaseManager,
+    },
+    data() {
+        this.api = racRealmApi;
+        return {};
+    },
+    mounted() {
+        //
+    },
+    methods: {
+        //
+    },
+};
+</script>
