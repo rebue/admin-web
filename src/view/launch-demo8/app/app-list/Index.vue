364edf1cfa19ea1ad389dd93ca8cd60d6b736216
罗先基 <2576439230@qq.com>
2021-12-06 10:40:42 +0800

    demo8-应用程序

--- src/view/launch-demo8/app/app-list/Index.vue
+++ src/view/launch-demo8/app/app-list/Index.vue
@@ -1,6 +1,33 @@
 <template>
     <div>
-        3.1应用列表
+        <fragment>
+            <a-card :bordered="false">
+                <a-button class="btn" type="link" icon="redo">
+                    刷新
+                </a-button>
+                <a-button class="btn" type="link" icon="file">
+                    管理我的模板
+                </a-button>
+                <a-collapse v-model="activeKey">
+                    <a-collapse-panel key="1" header="公共应用">
+                        <a-button class="btn" icon="setting">
+                            登录网站后台
+                        </a-button>
+                        <a-button class="btn" icon="book">
+                            图书
+                        </a-button>
+                        <a-button class="btn" icon="plus">
+                            点击新增
+                        </a-button>
+                    </a-collapse-panel>
+                    <a-collapse-panel key="2" header="个人应用" :disabled="false">
+                        <a-button class="btn" icon="plus">
+                            点击新增
+                        </a-button>
+                    </a-collapse-panel>
+                </a-collapse>
+            </a-card>
+        </fragment>
     </div>
 </template>
 
@@ -12,6 +39,7 @@ export default {
     data() {
         return {
             baseUrl: process.env.BASE_URL,
+            activeKey: ['1'],
         };
     },
     computed: {},
@@ -24,4 +52,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+    margin-right: 10px;
+}
+</style>
--- src/view/launch-demo8/app/app-option/Index.vue
+++ src/view/launch-demo8/app/app-option/Index.vue
@@ -1,6 +1,67 @@
 <template>
     <div>
-        3.2挂接系统管理
+        <fragment>
+            <a-card>
+                <a-row>
+                    <a-col :span="4">
+                        <a-tree
+                            class="ant-card-body"
+                            v-model="checkedKeys"
+                            show-icon
+                            :auto-expand-parent="autoExpandParent"
+                            :default-selected-keys="selectedKeys"
+                            :default-checked-keys="checkedKeys"
+                            :default-expanded-keys="expandedKeys"
+                            :tree-data="treeData"
+                            @check="onCheck"
+                        >
+                            <a-icon slot="file" type="file-o" />
+                            <a-icon slot="setting" type="setting-o" />
+                            <a-icon slot="cloud-sync" type="cloud-sync-o" />
+                        </a-tree>
+                    </a-col>
+                    <a-col :span="20">
+                        <div class="btn-group">
+                            <a-button-group>
+                                <a-button icon="search"></a-button>
+                                <a-dropdown>
+                                    <a-menu slot="overlay">
+                                        <a-menu-item key="1">
+                                            1st item
+                                        </a-menu-item>
+                                    </a-menu>
+                                    <a-button>
+                                        新增
+                                        <a-icon type="down" />
+                                    </a-button>
+                                </a-dropdown>
+                                <a-button class="btn">编辑</a-button>
+                                <a-button class="btn">删除</a-button>
+                                <a-dropdown>
+                                    <a-menu slot="overlay">
+                                        <a-menu-item key="1">
+                                            1st item
+                                        </a-menu-item>
+                                    </a-menu>
+                                    <a-button>
+                                        下载
+                                        <a-icon type="down" />
+                                    </a-button>
+                                </a-dropdown>
+                            </a-button-group>
+                        </div>
+                        <div>
+                            <a-table
+                                :columns="columns"
+                                :data-source="tableData"
+                                :row-selection="rowSelection"
+                                :expanded-row-keys.sync="expandedRowKeys"
+                            />
+                        </div>
+                    </a-col>
+                </a-row>
+            </a-card>
+        </fragment>
     </div>
 </template>
 
@@ -10,18 +71,98 @@ export default {
     components: {},
     props: [],
     data() {
+        const treeData = [
+            {
+                title: '普通模板挂接',
+                key: '1',
+                slots: {
+                    icon: 'file',
+                },
+            },
+            {
+                title: 'XRPC协议连接',
+                key: '2',
+                slots: {
+                    icon: 'setting',
+                },
+            },
+            {
+                title: '自定义数同步',
+                key: '3',
+                slots: {
+                    icon: 'cloud-sync',
+                },
+            },
+        ];
+        const columns = [
+            {
+                title: '模板名称',
+                dataIndex: 'name',
+                key: 'name',
+            },
+        ];
+        const tableData = [
+            {
+                key: 1,
+                name: '图书馆',
+            },
+            {
+                key: 2,
+                name: '登录网后台',
+            },
+            {
+                key: 3,
+                name: '新浪',
+            },
+            {
+                key: 4,
+                name: '123',
+            },
+        ];
+        const rowSelection = {
+            onChange: (selectedRowKeys, selectedRows) => {
+                console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+            },
+            onSelect: (record, selected, selectedRows) => {
+                console.log(record, selected, selectedRows);
+            },
+            onSelectAll: (selected, selectedRows, changeRows) => {
+                console.log(selected, selectedRows, changeRows);
+            },
+        };
         return {
             baseUrl: process.env.BASE_URL,
+            tableData,
+            treeData,
+            columns,
+            rowSelection,
+            expandedRowKeys: [],
+            autoExpandParent: true,
+            selectedKeys: [],
+            checkedKeys: [],
+            expandedKeys: ['2018', '20181'],
         };
     },
     computed: {},
     // created() {},
     // mounted() {},
     // destroyed() {},
-    methods: {},
+    methods: {
+        onCheck(checkedKeys, info) {
+            console.log('onCheck', checkedKeys, info);
+        },
+        onSelect(selectedKeys, info) {
+            console.log('onSelect', info);
+            this.selectedKeys = selectedKeys;
+        },
+    },
     watch: {},
     filters: {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn-group {
+    margin-bottom: 10px;
+}
+</style>
