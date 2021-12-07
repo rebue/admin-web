64f7a03e92034a69f87cf1988783eaf8d5b1d267
钟珍梅 <761780789@qq.com>
2021-12-07 10:56:51 +0800

    系统8-发送短息调整样式

--- src/view/launch-demo8/message/message-post/Index.vue
+++ src/view/launch-demo8/message/message-post/Index.vue
@@ -3,91 +3,89 @@
         <base-manager ref="baseManager">
             <template #managerCard>
                 <div>
-                    <div class="title">
-                        <div class="header-top">
-                            <span>注意提示</span>
-                        </div>
-                        <div class="header-button">
-                            <span
-                                >短信发送字数不得超过350字，少于70字算一条短信，多于70个字67字计为一条短信，总条数需要加上签名字数，实际条数按照服务器返回结果为准</span
-                            >
-                            <div class="left">
-                                <div class="left-child1">
-                                    <span class="ziti"><strong>短信批次名称</strong></span>
-                                </div>
-                                <div class="left-child2">
-                                    <input
-                                        style="width: 98%;height: 20px;background-color: #D9D9D9;"
-                                        placeholder="非必填,默认以发送时间为批次名"
-                                    />
-                                </div>
-                                <div class="left-child1">
-                                    <span class="ziti"><strong>院内收信人</strong></span>
-                                </div>
-                                <div style="height: 20px;"></div>
-                                <div class="left-child1">
-                                    <span class="ziti"><strong>自定义收信号码</strong></span>
-                                </div>
-                                <div class="left-child3">
-                                    <textarea
-                                        style="width: 98%;height: 70px;overflow: auto;"
-                                        placeholder="自定义电话号码以英文逗号分隔"
-                                    ></textarea>
-                                </div>
-                                <div class="left-child1">
-                                    <span class="ziti"
-                                        ><strong>短信内容<span style="color: red;">(必填)</span></strong></span
-                                    >
-                                    <span class="ziti" style="float: right;padding-right: 5px;"
-                                        ><strong>短信模板</strong></span
-                                    >
-                                </div>
-                                <div class="left-child3">
-                                    <textarea
-                                        style="width: 98%;height: 150%;max-width: 98%;max-height: 150px;overflow: auto;"
-                                        placeholder="自定义电话号码以英文逗号分隔"
-                                    ></textarea>
-                                </div>
-                            </div>
-                            <div class="right">
-                                <div class="right-child1">
-                                    <ul>
-                                        <li><a>教职工</a></li>
-                                        <li><a>学员</a></li>
-                                        <li><a>自定义</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="right-child2">
-                                    <input
-                                        type="text"
-                                        placeholder="请输入您要查找的部门"
-                                        style="width:390px;height: 25px;"
-                                    />
-                                </div>
-                                <div class="showBtn">
-                                    <div class="right-child3">
-                                        <a>校(院)领导<span class="btn">全选</span></a>
-                                    </div>
-                                    <div class="right-child3">
-                                        <a>办公室(业务指导工作处)<span class="btn">全选</span></a>
-                                    </div>
-                                    <div class="right-child3">
-                                        <a>组织人事处<span class="btn">全选</span></a>
-                                    </div>
-                                    <div class="right-child3">
-                                        <a>机关党委<span class="btn">全选</span></a>
-                                    </div>
-                                    <div class="right-child3">
-                                        <a>财务处<span class="btn">全选</span></a>
-                                    </div>
-                                    <div class="right-child3">
-                                        <a>资源管理处<span class="btn">全选</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div>
+                        <div class="header-top">注意提示</div>
+                        <p class="header-button">
+                            短信发送字数不得超过350字，少于70字算一条短信，多于70个字67字计为一条短信，总条数需要加上签名字数，实际条数按照服务器返回结果为准
+                        </p>
                     </div>
+                    <a-row type="flex">
+                        <a-col :flex="4" class="left" style="margin-right:40px">
+                            <div class="left-child1">
+                                <span class="ziti"><strong>短信批次名称</strong></span>
+                            </div>
+                            <div class="left-child2">
+                                <input
+                                    style="width: 98%;height: 20px;background-color: #D9D9D9;"
+                                    placeholder="非必填,默认以发送时间为批次名"
+                                />
+                            </div>
+                            <div class="left-child1">
+                                <span class="ziti"><strong>院内收信人</strong></span>
+                            </div>
+                            <div style="height: 20px;"></div>
+                            <div class="left-child1">
+                                <span class="ziti"><strong>自定义收信号码</strong></span>
+                            </div>
+                            <div class="left-child3">
+                                <textarea
+                                    style="width: 98%;height: 70px;overflow: auto;"
+                                    placeholder="自定义电话号码以英文逗号分隔"
+                                ></textarea>
+                            </div>
+                            <div class="left-child1">
+                                <span class="ziti"
+                                    ><strong>短信内容<span style="color: red;">(必填)</span></strong></span
+                                >
+                                <span class="ziti" style="float: right;padding-right: 5px;"
+                                    ><strong>短信模板</strong></span
+                                >
+                            </div>
+                            <div class="left-child3">
+                                <textarea
+                                    style="width: 98%;height: 150%;max-width: 98%;max-height: 150px;overflow: auto;"
+                                    placeholder="自定义电话号码以英文逗号分隔"
+                                ></textarea>
+                            </div>
+                        </a-col>
+                        <a-col :flex="1" class="right">
+                            <div class="right-child1">
+                                <ul>
+                                    <li><a>教职工</a></li>
+                                    <li><a>学员</a></li>
+                                    <li><a>自定义</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="right-child2">
+                                <input
+                                    type="text"
+                                    placeholder="请输入您要查找的部门"
+                                    style="width:390px;height: 25px;"
+                                />
+                            </div>
+                            <div class="showBtn">
+                                <div class="right-child3">
+                                    <a>校(院)领导<span class="btn">全选</span></a>
+                                </div>
+                                <div class="right-child3">
+                                    <a>办公室(业务指导工作处)<span class="btn">全选</span></a>
+                                </div>
+                                <div class="right-child3">
+                                    <a>组织人事处<span class="btn">全选</span></a>
+                                </div>
+                                <div class="right-child3">
+                                    <a>机关党委<span class="btn">全选</span></a>
+                                </div>
+                                <div class="right-child3">
+                                    <a>财务处<span class="btn">全选</span></a>
+                                </div>
+                                <div class="right-child3">
+                                    <a>资源管理处<span class="btn">全选</span></a>
+                                </div>
+                            </div>
+                        </a-col>
+                    </a-row>
                 </div>
             </template>
         </base-manager>
@@ -110,16 +108,12 @@ export default {
 * {
     font-family: '微软雅黑';
 }
-.title {
-    height: 60px;
-}
+
 .header-top {
     height: 30px;
     background-color: #ffe4e1;
     padding: 5px;
     line-height: 25px;
-    width: 1200px;
-    margin: 0 auto;
     border: 1px solid #ffe4e1;
 }
 .header-top span {
@@ -128,10 +122,7 @@ export default {
 }
 .header-button {
     height: 30px;
-
     padding: 5px;
-    width: 1200px;
-    margin: 0 auto;
     position: relative;
     border: 1px solid #ffe4e1;
 }
@@ -140,19 +131,9 @@ export default {
     padding-left: 10px;
 }
 .left {
-    width: 500px;
-    height: 380px;
-    margin-top: 50px;
-    margin-left: -500px;
     border: 1px solid #8cc0f7;
-    position: absolute;
 }
 .right {
-    width: 420px;
-    margin-top: 50px;
-    height: 380px;
-    position: absolute;
-    right: 50px;
     border: 1px solid #8cc0f7;
 }
 .left-child1 {
