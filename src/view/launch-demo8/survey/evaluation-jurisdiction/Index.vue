cf3fdf7ac52845794fb82a27ac33c45dff2dcc80
lsl <2530364891@qq.com>
2021-12-07 09:09:30 +0800

    更新页面

--- src/view/launch-demo8/survey/evaluation-jurisdiction/Index.vue
+++ src/view/launch-demo8/survey/evaluation-jurisdiction/Index.vue
@@ -2,80 +2,85 @@
     <fragment>
         <base-manager ref="baseManager">
             <template #managerCard>
-                <div style="width: 200px;min-height: 700px;float: left;margin-left: -24px;margin-top:-24px;">
-                    <ul class="left-btn">
-                        <li><a>后勤服务中心意见征询表</a></li>
-                        <li><a>新学员调查问卷</a></li>
-                        <li><a>事业单位党建工作调查问卷</a></li>
-                        <li><a>险地求生</a></li>
-                        <li><a>广西区党校工作者”职业幸福“</a></li>
-                        <li><a>”感“调查问卷</a></li>
-                    </ul>
-                </div>
+                <a-row>
+                    <a-col span="4">
+                        <div style="width: 200px;min-height: 700px;float: left;margin-left: -24px;margin-top:-24px;">
+                            <ul class="left-btn">
+                                <li><a>后勤服务中心意见征询表</a></li>
+                                <li><a>新学员调查问卷</a></li>
+                                <li><a>事业单位党建工作调查问卷</a></li>
+                                <li><a>险地求生</a></li>
+                                <li><a>广西区党校工作者”职业幸福“</a></li>
+                                <li><a>”感“调查问卷</a></li>
+                            </ul>
+                        </div>
+                    </a-col>
+                    <a-col span="20">
+                        <div
+                            style="float: left;margin-top: -20px;margin-left: 10px;width: 1000px;border-left:2px solid rgb(213,213,213);overflow: hidden"
+                        >
+                            <p>问卷<span style="color:#2d59b3;">[后勤服务中信意见征询表]</span>权限分组</p>
+                            <div
+                                style="height: 300px;width: 1000px;background-color: rgb(239,239,239);overflow: hidden;overflow: auto"
+                            >
+                                <a-tree
+                                    style="margin-left: -30px;margin-top: -20px"
+                                    class="ant-card-body"
+                                    v-model="checkedKeys"
+                                    :default-selected-keys="selectedKeys"
+                                    :default-checked-keys="checkedKeys"
+                                    @Click="onCheck"
+                                    :tree-data="treeData"
+                                />
+                            </div>
+                            <p>
+                                <span style="color:#2d59b3;">[平时考勤管理员]</span><span>分组权限</span
+                                ><span>&nbsp;查看分组成员</span>
+                            </p>
 
-                <div
-                    style="float: left;margin-top: -20px;margin-left: 10px;width: 1000px;border-left:2px solid rgb(213,213,213);overflow: hidden"
-                >
-                    <p>问卷<span style="color:#2d59b3;">[后勤服务中信意见征询表]</span>权限分组</p>
-                    <div
-                        style="height: 300px;width: 1000px;background-color: rgb(239,239,239);overflow: hidden;overflow: auto"
-                    >
-                        <a-tree
-                            style="margin-left: -30px;margin-top: -20px"
-                            class="ant-card-body"
-                            v-model="checkedKeys"
-                            :default-selected-keys="selectedKeys"
-                            :default-checked-keys="checkedKeys"
-                            @Click="onCheck"
-                            :tree-data="treeData"
-                        />
-                    </div>
-                    <p>
-                        <span style="color:#2d59b3;">[平时考勤管理员]</span><span>分组权限</span
-                        ><span>&nbsp;查看分组成员</span>
-                    </p>
-
-                    <table style="line-height: 25px">
-                        <tr>
-                            <td style="width: 500px">结果查看</td>
-                            <td style="width: 200px"><input type="radio" name="name1" />允许</td>
-                            <td style="width: 200px"><input type="radio" name="name1" />拒绝</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 500px">结果修改</td>
-                            <td style="width: 200px"><input type="radio" name="name2" />允许</td>
-                            <td style="width: 200px"><input type="radio" name="name2" />拒绝</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 500px">问卷设置</td>
-                            <td style="width: 200px"><input type="radio" name="name3" />允许</td>
-                            <td style="width: 200px"><input type="radio" name="name3" />拒绝</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 500px">新增问卷</td>
-                            <td style="width: 200px"><input type="radio" name="name4" />允许</td>
-                            <td style="width: 200px"><input type="radio" name="name4" />拒绝</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 500px">内容查看</td>
-                            <td style="width: 200px"><input type="radio" name="name5" />允许</td>
-                            <td style="width: 200px"><input type="radio" name="name5" />拒绝</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 500px">新增内容</td>
-                            <td style="width: 200px"><input type="radio" name="name6" />允许</td>
-                            <td style="width: 200px"><input type="radio" name="name6" />拒绝</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 500px">设置权限</td>
-                            <td style="width: 200px"><input type="radio" name="name7" />允许</td>
-                            <td style="width: 200px"><input type="radio" name="name7" />拒绝</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 500px"><input type="checkbox" />关联设置</td>
-                        </tr>
-                    </table>
-                </div>
+                            <table style="line-height: 25px">
+                                <tr>
+                                    <td style="width: 500px">结果查看</td>
+                                    <td style="width: 200px"><input type="radio" name="name1" />允许</td>
+                                    <td style="width: 200px"><input type="radio" name="name1" />拒绝</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 500px">结果修改</td>
+                                    <td style="width: 200px"><input type="radio" name="name2" />允许</td>
+                                    <td style="width: 200px"><input type="radio" name="name2" />拒绝</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 500px">问卷设置</td>
+                                    <td style="width: 200px"><input type="radio" name="name3" />允许</td>
+                                    <td style="width: 200px"><input type="radio" name="name3" />拒绝</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 500px">新增问卷</td>
+                                    <td style="width: 200px"><input type="radio" name="name4" />允许</td>
+                                    <td style="width: 200px"><input type="radio" name="name4" />拒绝</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 500px">内容查看</td>
+                                    <td style="width: 200px"><input type="radio" name="name5" />允许</td>
+                                    <td style="width: 200px"><input type="radio" name="name5" />拒绝</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 500px">新增内容</td>
+                                    <td style="width: 200px"><input type="radio" name="name6" />允许</td>
+                                    <td style="width: 200px"><input type="radio" name="name6" />拒绝</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 500px">设置权限</td>
+                                    <td style="width: 200px"><input type="radio" name="name7" />允许</td>
+                                    <td style="width: 200px"><input type="radio" name="name7" />拒绝</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 500px"><input type="checkbox" />关联设置</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a-col>
+                </a-row>
             </template>
         </base-manager>
     </fragment>
--- src/view/launch-demo8/survey/evaluation-result/Index.vue
+++ src/view/launch-demo8/survey/evaluation-result/Index.vue
@@ -2,64 +2,71 @@
     <fragment>
         <base-manager ref="baseManager">
             <template #managerCard>
-                <div
-                    style="width: 200px;min-height: 700px;float: left;margin-left: -24px;margin-top:-24px;border-right:2px solid rgb(213,213,213)"
-                >
-                    <ul class="left-btn">
-                        <li><a>新学员调查问卷</a></li>
-                        <li><a>险地求生</a></li>
-                        <li><a>事业单位党建工作调查问卷</a></li>
-                        <li><a>后勤服务中心意见征询表</a></li>
-                        <li><a>广西党校工作者”职业幸福感“</a></li>
-                        <li><a>”感“调查问卷</a></li>
-                    </ul>
-                </div>
-
-                <div style="float: left;margin-top: -20px;margin-left: 10px;width: 1000px">
-                    <a-tabs default-active-key="1" @change="callback">
-                        <a-tab-pane key="1" tab="问卷调查">
-                            <div>
-                                <a-button>新增</a-button>
-                                <a-button>删除</a-button>
-                                <a-button>导出</a-button>
-                            </div>
-                            <div
-                                v-show="showOrg"
-                                style="margin-right: 10px;width:200px;margin-top: 5px;float: left;"
-                                class="table-left"
-                            >
-                                <org-tree
-                                    :ref="`orgTree.platform`"
-                                    :show.sync="showOrg"
-                                    realmId="platform"
-                                    @click="handleOrgMenuClick"
-                                    @select="handleOrgTreeSelect"
-                                />
-                            </div>
-                            <div style="float: right;width: 790px;border-top: 1px solid rgb(213,213,213)">
-                                <p style="float: right;color: red;">
-                                    测评开放时间:2021-12-06 00:00:00 至 2021-12-31 23:59:59
-                                </p>
-                                <div style="width: 790px;overflow: hidden">
-                                    <center>
-                                        <h1>新学员调查问卷</h1>
-                                    </center>
-                                    <div style="margin-top: 50px">
-                                        <p style="font-size: 17px">您来党校的最关注的问题是什么？ （一百字以内）</p>
-                                        说明:广西区党校开展的晚簸习活动，是由学员利用部分晚自习时间，目行组织开展的形式多样、自由灵活的团队学习模式，旨在为学员推供充分学习、交流和实训的平台，推指字项的球导力。其中的“求是社区”旨在围绕学员共同关心关注的问题分社区和小组进行团体性研讨交流、分摩经验，寻找破解难题之策，为党委、政府或个人工作提供决策参考。请你就诸如“你最关往的基层实际问题是什么“、”你在实际工作中遇到的最需要研讨的问题是什么“、”你到党校学习期间最希望研讨交流并得到解执或启示的问题是什么”"和“学习1.大后你最关注的间题又是什么”等问题法行思考，并把你思考的结果填人此表。并请你在人学报到时上交。
-                                        <br />
-                                        <a-textarea
-                                            style="height: 100px;max-height: 100px;background-color: rgb(235,235,228)"
-                                        ></a-textarea>
+                <a-row>
+                    <a-col span="4">
+                        <div
+                            style="width: 200px;min-height: 700px;float: left;margin-left: -24px;margin-top:-24px;border-right:2px solid rgb(213,213,213)"
+                        >
+                            <ul class="left-btn">
+                                <li><a>新学员调查问卷</a></li>
+                                <li><a>险地求生</a></li>
+                                <li><a>事业单位党建工作调查问卷</a></li>
+                                <li><a>后勤服务中心意见征询表</a></li>
+                                <li><a>广西党校工作者”职业幸福感“</a></li>
+                                <li><a>”感“调查问卷</a></li>
+                            </ul>
+                        </div>
+                    </a-col>
+                    <a-col span="20">
+                        <div style="float: left;margin-top: -20px;width: 1000px">
+                            <a-tabs default-active-key="1" @change="callback">
+                                <a-tab-pane key="1" tab="问卷调查">
+                                    <div>
+                                        <a-button>新增</a-button>
+                                        <a-button>删除</a-button>
+                                        <a-button>导出</a-button>
                                     </div>
-                                </div>
-                                <center>
-                                    <a-button type="primary" style="margin-top: 20px">提交</a-button>
-                                </center>
-                            </div>
-                        </a-tab-pane>
-                    </a-tabs>
-                </div>
+                                    <div
+                                        v-show="showOrg"
+                                        style="margin-right: 10px;width:200px;margin-top: 5px;float: left;"
+                                        class="table-left"
+                                    >
+                                        <org-tree
+                                            :ref="`orgTree.platform`"
+                                            :show.sync="showOrg"
+                                            realmId="platform"
+                                            @click="handleOrgMenuClick"
+                                            @select="handleOrgTreeSelect"
+                                        />
+                                    </div>
+                                    <div style="float: right;width: 790px;border-top: 1px solid rgb(213,213,213)">
+                                        <p style="float: right;color: red;">
+                                            测评开放时间:2021-12-06 00:00:00 至 2021-12-31 23:59:59
+                                        </p>
+                                        <div style="width: 790px;overflow: hidden">
+                                            <center>
+                                                <h1>新学员调查问卷</h1>
+                                            </center>
+                                            <div style="margin-top: 50px">
+                                                <p style="font-size: 17px">
+                                                    您来党校的最关注的问题是什么？ （一百字以内）
+                                                </p>
+                                                说明:广西区党校开展的晚簸习活动，是由学员利用部分晚自习时间，目行组织开展的形式多样、自由灵活的团队学习模式，旨在为学员推供充分学习、交流和实训的平台，推指字项的球导力。其中的“求是社区”旨在围绕学员共同关心关注的问题分社区和小组进行团体性研讨交流、分摩经验，寻找破解难题之策，为党委、政府或个人工作提供决策参考。请你就诸如“你最关往的基层实际问题是什么“、”你在实际工作中遇到的最需要研讨的问题是什么“、”你到党校学习期间最希望研讨交流并得到解执或启示的问题是什么”"和“学习1.大后你最关注的间题又是什么”等问题法行思考，并把你思考的结果填人此表。并请你在人学报到时上交。
+                                                <br />
+                                                <a-textarea
+                                                    style="height: 100px;max-height: 100px;background-color: rgb(235,235,228)"
+                                                ></a-textarea>
+                                            </div>
+                                        </div>
+                                        <center>
+                                            <a-button type="primary" style="margin-top: 20px">提交</a-button>
+                                        </center>
+                                    </div>
+                                </a-tab-pane>
+                            </a-tabs>
+                        </div>
+                    </a-col>
+                </a-row>
             </template>
         </base-manager>
     </fragment>
