570710c0c09563a8feb6e863bad524bd55df06af
罗先基 <2576439230@qq.com>
2021-12-08 09:57:43 +0800

    更新demo8-应用程序

--- src/view/launch-demo8/app/app-list/Index.vue
+++ src/view/launch-demo8/app/app-list/Index.vue
@@ -1,33 +1,25 @@
 <template>
-    <div>
-        <fragment>
-            <a-card :bordered="false">
-                <a-button class="btn" type="link" icon="redo">
-                    刷新
-                </a-button>
-                <a-button class="btn" type="link" icon="file">
-                    管理我的模板
-                </a-button>
-                <a-collapse v-model="activeKey">
-                    <a-collapse-panel key="1" header="公共应用">
-                        <a-button class="btn" icon="setting">
-                            登录网站后台
-                        </a-button>
-                        <a-button class="btn" icon="book">
-                            图书
-                        </a-button>
-                        <a-button class="btn" icon="plus">
-                            点击新增
-                        </a-button>
-                    </a-collapse-panel>
-                    <a-collapse-panel key="2" header="个人应用" :disabled="false">
-                        <a-button class="btn" icon="plus">
-                            点击新增
-                        </a-button>
-                    </a-collapse-panel>
-                </a-collapse>
-            </a-card>
-        </fragment>
+    <div class="w100 pt20 pb20 pl20 pr20 bg-w radius-4 card-shadow">
+        <a-button type="link" icon="redo">
+            刷新
+        </a-button>
+        <a-button type="link" icon="file">
+            管理我的模板
+        </a-button>
+        <div class="w100" v-for="(item, index) in appList" :key="index">
+            <a-collapse>
+                <a-collapse-panel :key="index" :header="item.title">
+                    <a-row class="mt20 pl20 pr20" :gutter="20">
+                        <a-col class="mt10 mb10" :span="4" v-for="(val, key) in item.list" :key="key">
+                            <router-link class="w100 in-block tx-c" :to="val.to">
+                                <a-icon :type="val.icon" theme="twoTone" :style="{ fontSize: '48px' }" />
+                                <p class="w100 mt10 tx-c font-14 item-title">{{ val.name }}</p>
+                            </router-link>
+                        </a-col>
+                    </a-row>
+                </a-collapse-panel>
+            </a-collapse>
+        </div>
     </div>
 </template>
 
@@ -39,7 +31,39 @@ export default {
     data() {
         return {
             baseUrl: process.env.BASE_URL,
-            activeKey: ['1'],
+
+            appList: [
+                {
+                    title: '公共应用',
+                    list: [
+                        {
+                            name: '登录网站后台',
+                            icon: 'appstore',
+                            to: '',
+                        },
+                        {
+                            name: '图书',
+                            icon: 'book',
+                            to: '',
+                        },
+                        {
+                            name: '点击新增',
+                            icon: 'file-add',
+                            to: '',
+                        },
+                    ],
+                },
+                {
+                    title: '个人应用',
+                    list: [
+                        {
+                            name: '点击新增',
+                            icon: 'file-add',
+                            to: '',
+                        },
+                    ],
+                },
+            ],
         };
     },
     computed: {},
@@ -52,8 +76,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.btn {
-    margin-right: 10px;
-}
-</style>
+<style scoped></style>
