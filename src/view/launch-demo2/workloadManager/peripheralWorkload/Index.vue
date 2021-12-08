75981636a94f98544a784110ec0c4cbf7052bf92
lsl <2530364891@qq.com>
2021-12-08 16:54:41 +0800

    提交demo2

---
+++ src/view/launch-demo2/workloadManager/peripheralWorkload/Index.vue
@@ -0,0 +1,255 @@
+<template>
+    <fragment>
+        <base-manager ref="baseManager">
+            <template #managerCard>
+                <crud-table
+                    ref="crudTable"
+                    :commands="tableCommands"
+                    :actions="tableActions"
+                    :columns="columns"
+                    :api="api"
+                    :showKeywords="true"
+                    :scrollX="600"
+                    :defaultPagination="false"
+                >
+                </crud-table>
+            </template>
+        </base-manager>
+    </fragment>
+</template>
+
+<script>
+import BaseManager from '@/component/rebue/BaseManager';
+import CrudTable from '@/component/rebue/CrudTable.vue';
+import { EditFormTypeDic } from '@/dic/EditFormTypeDic';
+
+export default {
+    name: 'ClassFund',
+    components: {
+        BaseManager,
+        CrudTable,
+    },
+    data() {
+        // 初始化数据start
+        const page = function() {
+            const p = new Promise(resolve => {
+                // const Mock = require('mockjs');
+                const mockList = require('mockjs').mock({
+                    // 属性 list 的值是一个数组，其中含有 1 到 3 个元素
+                    'list|5': [
+                        {
+                            'teacherNo|+1': 10000000,
+                            'workloadName|+1': ['组织学生进行大扫除', '开展班级文化交流活动', '各班进行一次交流授课'],
+                            workloadTime: '@now(yyyy-MM-dd)',
+                            department: ['行政部'],
+                            createTime: '@now(yyyy-MM-dd)',
+                            endTime: '@now(yyyy-MM-dd)',
+                            'explain|+1': ['清洁校园，搞好卫生', '促进各班直接师生感情', '对教学质量的探讨'],
+                        },
+                    ],
+                });
+                // 数据列表在这里设置
+                for (let i = 0; i < mockList.list.length; i++) {
+                    mockList.list[i].monthWorkload = Math.round(mockList.list[i].yearWorkload / 12);
+                }
+                const dataSource = mockList.list;
+                const ro = {
+                    extra: {
+                        page: {
+                            list: dataSource,
+                            total: 20,
+                        },
+                        list: dataSource,
+                    },
+                };
+                resolve(ro);
+            });
+
+            return p;
+        };
+
+        this.api = {
+            page,
+            listAll: page,
+            list: page,
+        };
+        const columns = [
+            {
+                dataIndex: 'workloadName',
+                title: '工作量名称',
+                width: 200,
+            },
+            {
+                dataIndex: 'workloadTime',
+                title: '工作量时间',
+                width: 150,
+            },
+            {
+                dataIndex: 'department',
+                title: '归属部门',
+                width: 150,
+            },
+            {
+                dataIndex: 'createTime',
+                title: '统计起始时间',
+                ellipsis: true,
+                width: 150,
+            },
+            {
+                dataIndex: 'endTime',
+                title: '统计结束时间',
+                ellipsis: true,
+                width: 150,
+            },
+            {
+                dataIndex: 'explain',
+                title: '说明',
+                ellipsis: true,
+                width: 200,
+            },
+        ];
+
+        this.tableCommands = [
+            {
+                buttonType: 'primary',
+                icon: 'plus',
+                title: '新增',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                icon: 'plus',
+                title: '编辑',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                icon: 'plus',
+                title: '删除',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                icon: 'plus',
+                title: '导入',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                icon: 'plus',
+                title: '导出',
+                onClick: this.handleAdd,
+            },
+            {
+                buttonType: 'primary',
+                icon: 'plus',
+                title: '工作量分值换算',
+                onClick: this.handleAdd,
+            },
+        ];
+
+        this.tableActions = [
+            {
+                type: 'a',
+                title: '编辑',
+                onClick: record => this.handleEdit(record),
+            },
+            {
+                type: 'confirm',
+                title: '删除',
+                confirmTitle: '你确定要删除本条记录吗?',
+                onClick: record => this.handleDel(record),
+            },
+        ];
+        return {
+            loading: false,
+            curRealmId: '',
+            manageMenusFormVisible: false,
+            curApp: {},
+            realms: [],
+            columns,
+            showOrg: true,
+        };
+    },
+    mounted() {
+        this.editForm = this.$refs.editForm;
+        this.refreshData();
+    },
+    methods: {
+        refreshData() {
+            this.loading = true;
+            // racRealmApi
+            //     .listAll()
+            //     .then(ro => {
+            //         this.realms = ro.extra.list;
+            //         this.curRealmId = this.realms[0].id;
+            //     })
+            //     .finally(() => (this.loading = false));
+        },
+        /**
+         * 刷新表格数据
+         */
+        refreshTableData() {
+            this.crudTable.refreshData();
+        },
+        handleRealmChanged(realmId) {
+            this.curRealmId = realmId;
+        },
+        /**
+         * 处理添加应用的事件
+         */
+        handleAdd() {
+            this.editForm.show(EditFormTypeDic.Add, {
+                realmId: this.curRealmId,
+            });
+        },
+        /**
+         * 处理编辑应用的事件
+         */
+        handleEdit(record) {
+            this.editForm.show(EditFormTypeDic.Modify, record);
+        },
+        /**
+         * 处理删除应用的事件
+         */
+        handleDel(record) {
+            this.loading = true;
+            this.api
+                .delById(record.id)
+                .then(() => {
+                    this.refreshTableData();
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+        /**
+         * 处理应用菜单的事件
+         */
+        handleMenus(record) {
+            this.curApp = record;
+            this.manageMenusFormVisible = true;
+        },
+        handleEditFormClose() {
+            this.refreshTableData();
+        },
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.realm-tabs {
+    overflow: visible; /* 否则表格的分页选择框展开时会被遮挡 */
+}
+
+.table-left {
+    display: flex;
+    height: 100%;
+    margin: 4px 0;
+    .table-divider {
+        width: 20px;
+        border-left: 1px solid #eee;
+        margin-left: 10px;
+    }
+}
+</style>
